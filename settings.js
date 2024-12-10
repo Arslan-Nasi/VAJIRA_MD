4ad51f7f5bc0f59ace5dc3fdf19d7972e4336349
Arslan-Nasi <119528303+Arslan-Nasi@users.noreply.github.com>
2024-12-10 14:24:11 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,14 +6,14 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? 'Put your session id here' : process.env.SESSION_ID,
-OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94711453361' : process.env.OWNER_NUMBER,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=CYNSVLob#juBgk8glG7wKfuhjzedNKxyFz6rVEDo1zDhhGhIRdns' : process.env.SESSION_ID,
+OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '923012995636' : process.env.OWNER_NUMBER,
 N_JID: process.env.N_JID=== undefined ? '‌': process.env.N_JID,    
 PREFIX: process.env.PREFIX || '.' ,
-FOOTER: process.env.FOOTER=== undefined ? '‌👨‍💻 ᴠᴀᴊɪʀᴀ ᴍᴅ ʙʏ ᴛᴄ ᴛᴇᴀᴍ 👨‍💻': process.env.FOOTER,
+FOOTER: process.env.FOOTER=== undefined ? '‌👨‍💻 ✪⏤͟͞★⃝ī.am ꪶ𝐶𝑌𝐵𝛯𝑅 𝐿𝛯𝐺𝛯𝛮𝐷👑➺ ᴍᴅ ʙʏ ᴛᴄ ᴛᴇᴀᴍ 👨‍💻': process.env.FOOTER,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgres://vajiratech_user:oSIFl2xmSojMZ0rkzdd0g0W6msuVTpNN@dpg-cpd7fjv109ks73e5gtig-a.frankfurt-postgres.render.com/vajiratech' : process.env.POSTGRESQL_URL,   
-MAX_SIZE: 500,
+MAX_SIZE: 2000,
 PAIR: 'https://vajiraqrveb-1d064bcd8502.herokuapp.com/code?number=',       
-ALIVE:  process.env.ALIVE  || '> VAJIRA MD'  ,    
+ALIVE:  process.env.ALIVE  || '> ✪⏤͟͞★⃝ī.am ꪶ𝐶𝑌𝐵𝛯𝑅 𝐿𝛯𝐺𝛯𝛮𝐷👑➺'  ,    
 DELETEMSGSENDTO : process.env.DELETEMSGSENDTO === undefined ? '' : process.env.DELETEMSGSENDTO        
 };
